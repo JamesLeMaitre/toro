ca2a90aef1518594afa6a1f61a6aaf15f1fc239c
Amemorete <koisonnmanes@gmail.com>
2023-01-15 01:23:38 +0000

    -uj

--- src/main/java/com/torocommunication/torofull/controller/FormationController.java
+++ src/main/java/com/torocommunication/torofull/controller/FormationController.java
@@ -14,6 +14,7 @@ import static com.torocommunication.torofull.constants.MessageController.*;
 
 @RestController
 @RequestMapping("api/formation/")
+@CrossOrigin("*")
 public class FormationController extends DataFormatter<Formation> {
 
     private final FormationInterface formationInterface;
